bf6287cdc08afab62664aa69218193f386232110
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-10-20 00:05:49 +0300

    fts: speedup search

--- lib.fts.js
+++ lib.fts.js
@@ -1,21 +1,29 @@
 let minimist = require('minimist')
 
-// TODO: add filtering by date, authors, tags
-exports.query = function(db, argv) {
-    let fts = db.prepare('SELECT file,subject,date,snippet(fts,3,"<b>","</b>","...",64) AS snippet from fts WHERE fts.body MATCH ? ORDER BY rank LIMIT 100')
-    let metatags = db.prepare('SELECT type,name FROM metatags WHERE file=?')
-    return fts.all(argv._.join(' ')).map( post => {
-	let mt = metatags.all(post.file)
-	post.authors = mt.filter( v => v.type === 'author').map( v => v.name)
-	post.tags = mt.filter( v => v.type === 'tag').map( v => v.name)
-	return post
-    })
+class Finder {
+    constructor(db) { this.db = db }
+    metatags() {
+	if (this._metatags) return this._metatags
+	let st = this.db.prepare('SELECT * FROM metatags')
+	return this._metatags = st.all()
+    }
+    static argv_parse(argv) {
+	return minimist(argv, {
+	    string: ['d', 'a', 't', 'A', 'T', 'db' /* cli only */]
+	})
+    }
+    // FIXME: add -d, -t, -T, -a, -A support
+    find(argv) {
+	let fts = this.db.prepare('SELECT file,subject,date,snippet(fts,3,"<b>","</b>","...",64) AS snippet from fts WHERE fts.body MATCH ? ORDER BY rank LIMIT 100')
+	return fts.all(argv._.join(' ')).map( post => {
+	    let mt = this.metatags().filter( v => v.file === post.file)
+	    post.authors = mt.filter( v => v.type === 'author').map(v => v.name)
+	    post.tags = mt.filter( v => v.type === 'tag').map( v => v.name)
+	    return post
+	})
+    }
 }
 
-exports.argv_parse = function(argv) {
-    return minimist(argv, {
-	string: ['d', 'a', 't', 'A', 'T', 'db']
-    })
-}
+exports.Finder = Finder
 
 exports.help = `QUERY: [-d from-to] [-a author] [-A author] [-t tag] [-T tag] phrase`
