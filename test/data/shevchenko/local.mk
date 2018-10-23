c6d622f5a7ee36dec683b84bee51229c9398922b
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-10-24 00:56:47 +0300

    fts: integrate it into nbe-make-new-blog

--- test/data/shevchenko/local.mk
+++ test/data/shevchenko/local.mk
@@ -11,3 +11,37 @@ define help.local :=
 sync		rsync to $(sync.dest)
 endef
 help += $(help.local)
+
+
+
+out.fts := fts
+fts.db := $(out.fts)/db.sqlite3
+
+$(fts.db): $(out)/web/index.json
+	$(mkdir)
+	$(mk)/fts-create $< $@
+	git -C $(out.fts) init
+	git -C $(out.fts) add .
+	-git -C $(out.fts) commit -m upd
+
+fts.server.src := $(mk)/nbe-fts-server $(mk)/lib/fts.js
+fts.server.dest := $(patsubst $(mk)/%, $(out.fts)/%, $(fts.server.src))
+
+$(out.fts)/%: $(mk)/%
+	$(mkdir)
+	$(copy)
+
+.PHONY: fts-create
+fts-create: $(fts.db) $(fts.server.dest)
+
+# edit this
+.PHONY: fts-deploy
+fts-deploy: fts-kill fts-create
+	$(fts.server) &
+
+# edit this
+.PHONY: fts-deploy
+fts-kill:
+	-pkill -f '$(fts.server)'
+
+fts.server := node $(out.fts)/nbe-fts-server $(fts.db)
