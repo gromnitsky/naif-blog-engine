b334b32aa2c6bc7ad48d93599eace63d70dc9140
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-10-29 05:42:05 +0200

    graph: tiny refactorings

--- authors_graph.js
+++ authors_graph.js
@@ -11,36 +11,34 @@ document.addEventListener('DOMContentLoaded', () => {
 function plot(index) {
     if (author() === 'anonymous') return
 
-    let id = '#nbe__authors__graph'
-    let node = web.$(id)[0]
-    node.className = 'ct-double-octave'
+    let node = web.$('#nbe__authors__graph')[0]
     let tooltip = new Tooltip('#nbe__authors__graph__tooltip')
 
     let plot_data = points(common.index_group_by(index, 'authors')[author()])
     let data = { series: [ plot_data ] }
 
-    let ticks = (all, max_allowed) => {
-	if (all.length <= max_allowed) return all
-	return all.filter((v,idx) => idx % Math.ceil(all.length/max_allowed) === 0)
+    let ticks = (all, max) => {
+	if (all.length <= max) return all
+	return all.filter((v,idx) => idx % Math.ceil(all.length/max) === 0)
     }
-    let yyyy_mm = d => `${d.getFullYear()}-${d.getMonth()+1}`
+    let yyyy_m = d => `${d.getFullYear()}-${d.getMonth()+1}`
 
     new Chartist.Line(node, data, {
 	chartPadding: { bottom: 30 },
 	axisX: {
 	    type: Chartist.FixedScaleAxis,
 	    ticks: ticks(plot_data.map( v => v.x), 20),
-	    labelInterpolationFnc: n => yyyy_mm(new Date(n))
+	    labelInterpolationFnc: n => yyyy_m(new Date(n))
 	}
     }).on('draw', function(data) {
-	if (data.type === 'point') tooltip.register(data.element._node, `${yyyy_mm(new Date(data.value.x))} (${data.value.y})`)
+	if (data.type === 'point') tooltip.register(data.element._node, `${yyyy_m(new Date(data.value.x))} (${data.value.y})`)
     })
 }
 
 class Tooltip {
     constructor(id) { this.node = web.$(id)[0] }
     show(evt, text) {
-	this.node.innerHTML = common.e(text)
+	this.node.innerText = text
 	this.node.style.display = 'block'
 	this.node.style.left = evt.pageX + 10 + 'px'
 	this.node.style.top = evt.pageY + 10 + 'px'
